54f18a537501687220af140ae013a2e6c9619e1d
thisisommore <ommore501@gmail.com>
2023-02-21 17:42:26 +0000

    Introduce chat app program

--- chat-app/programs/chat-app/src/lib.rs
+++ chat-app/programs/chat-app/src/lib.rs
@@ -1,15 +1,111 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("8pW94nJtpxwHmNp1A9hAmTihwQgvGMrp8J5v9ACRsEdC");
 
 #[program]
 pub mod chat_app {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_user(ctx: Context<CreateUser>, encryption_key: String)-> Result<()> {
+        let new_user = &mut ctx.accounts.user_account;
+        new_user.encryption_key=encryption_key;
+        Ok(())
+    }
+    pub fn initialize(ctx: Context<Initialize>, content_hash: String) -> Result<()> {
+        let new_chat = &mut ctx.accounts.chat;
+        new_chat.participant_a = ctx.accounts.user.key.to_owned();
+        new_chat.participant_b = ctx.accounts.receiver.key.to_owned();
+        
+        let new_message = Message {
+            content_hash: content_hash,
+            is_first: true,
+        };
+        new_chat.messages = vec![new_message];
+        Ok(())
+    }
+
+    pub fn put_message(ctx: Context<PutMessage>, content_hash: String) -> Result<()> {
+        let chat_account = &mut ctx.accounts.chat;
+        let sender_pub_key = ctx.accounts.sender.key;
+        if &chat_account.participant_a != sender_pub_key
+            && &chat_account.participant_b != sender_pub_key
+        {
+            return err!(ErrorCode::NotParticipant);
+        }
+
+        let new_message = Message {
+            content_hash: content_hash,
+            is_first: &chat_account.participant_a==sender_pub_key,
+        };
+
+        if chat_account.messages.len()==0 {
+            chat_account.messages.remove(0);
+        }
+        chat_account.messages.push(new_message);
+
         Ok(())
     }
 }
 
+#[derive(AnchorSerialize,AnchorDeserialize,Clone)]
+pub struct Message {
+    pub is_first: bool,
+    pub content_hash: String,
+}
+
+// TODO recalculate all spaces
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateUser<'info> {
+    /// CHECK: max unsafe
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        space = 174, seeds = [b"user", user.key().as_ref()], bump
+    )]
+    pub user_account: Account<'info, User>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Chat {
+    pub participant_a: Pubkey,
+    pub participant_b: Pubkey,
+    pub messages: Vec<Message>,
+}
+
+#[account]
+pub struct User {
+    pub public_key:Pubkey,
+    pub encryption_key:String
+}
+
+#[derive(Accounts)]
+pub struct PutMessage<'info> {
+    pub chat: Account<'info, Chat>,
+    pub sender: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    /// CHECK: max unsafe
+    #[account(mut)]
+    pub user: Signer<'info>,
+    /// CHECK: max unsafe
+    #[account()]
+    pub receiver: UncheckedAccount<'info>,
+    #[account(
+        init,
+        payer = user,
+        space = 178, seeds = [b"user-chat", user.key().as_ref(), receiver.key().as_ref()], bump
+    )]
+    pub chat: Account<'info, Chat>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("You are not participant")]
+    NotParticipant,
+}
--- chat-app/tests/chat-app.ts
+++ chat-app/tests/chat-app.ts
@@ -1,16 +1,57 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { ChatApp } from "../target/types/chat_app";
+import {ChatApp} from "../target/types/chat_app";
 
 describe("chat-app", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.ChatApp as Program<ChatApp>;
 
+  const receiver = new anchor.web3.Keypair()
   it("Is initialized!", async () => {
+
+    const CHAT_SEED = "user-chat"
+
+    const [CHAT_PDA] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from(CHAT_SEED), provider.wallet.publicKey.toBuffer(), receiver.publicKey.toBuffer()],
+      program.programId
+    );
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.initialize("hemlo").accounts({
+      chat: CHAT_PDA,
+      user: provider.wallet.publicKey,
+      receiver:receiver.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).rpc();
   });
+
+  it("Should create user", async ()=>{
+    const USER_SEED = "user"
+    const [USER_PDA] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from(USER_SEED), provider.wallet.publicKey.toBuffer()],
+      program.programId
+    );
+
+    const tx = await program.methods.createUser("abcd").accounts({
+      userAccount: USER_PDA,
+      user: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).rpc();
+  })
+
+  it("Should put message", async ()=>{
+    const CHAT_SEED = "user-chat"
+
+    const [CHAT_PDA] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from(CHAT_SEED), provider.wallet.publicKey.toBuffer(), receiver.publicKey.toBuffer()],
+      program.programId
+    );
+
+    const tx = await program.methods.putMessage("abcd").accounts({
+      chat: CHAT_PDA,
+      sender: provider.wallet.publicKey,
+    }).rpc();
+  })
 });
